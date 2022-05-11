ca308cbe83895d8afc3aa13ab24594f681f67931
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-05-11 19:34:05 -0400

    commit tree ad1ec13379d81c53bb17f20f6726de9c31fca184

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(x){  
+    
+    const a = x.find(function(x){
+        return x.result==='W';
+    });
+    if(a !== undefined){
+        return a.year;
+    }
+
+}
+
+    
